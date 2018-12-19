2ceb3a1353db9aeb94dbd8f460b1e9761c2b933b
Mykolas Kaminskas <mykolaskam@gmail.com>
2018-12-19 16:54:09 +0000

    Added PDO connection settings

--- includes/secure_web_app/app/settings.php
+++ includes/secure_web_app/app/settings.php
@@ -17,9 +17,24 @@ $settings = [
       'twig' => [
 	'cache' => __DIR__ . '/cache/twig',
         'auto_reload' => true
-      ],
+      ]
     ],
-  ]
+      'pdo' => [
+          'rdbms' => 'mysql',
+          'host' => 'localhost',
+          'db_name' => 'secure_db',
+          'port' => '8888',
+          'user_name' => 'admin',
+          'user_password' => 'admin',
+          'charset' => 'utf8',
+          'collation' => 'utf8_unicode_ci',
+          'options' => [
+              PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+              PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+              PDO::ATTR_EMULATE_PREPARES   => true,
+          ],
+      ]
+  ],
 ];
 
 return $settings;
