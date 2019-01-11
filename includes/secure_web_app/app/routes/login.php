1251872b65dd7dd533fa12f4c94dddf84f6e87fd
Vinay <vintailor11@gmail.com>
2019-01-11 14:17:55 +0000

    changes to routing

--- includes/secure_web_app/app/routes/login.php
+++ includes/secure_web_app/app/routes/login.php
@@ -9,8 +9,8 @@ $app->get('/', function(Request $request, Response $response)
     return $this->view->render($response,
         'login.html.twig',
         [
-            'action_login' => '/SecureWebApp-master/SecureWebApp/public_php/SecureWebApp/index.php/logincheck',
-            'action_register' => '/SecureWebApp-master/SecureWebApp/public_php/SecureWebApp/index.php/register',
+            'action_login' => '/SecureWebApp/index.php/logincheck',
+            'action_register' => '/SecureWebApp/index.php/register',
 
         ]);
         
